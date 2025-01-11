c9d19ad6208ead98173ef1f849c3f623c173424f
DyKhang <128365839+DyKhang@users.noreply.github.com>
2025-01-12 02:24:43 +0700

    finish set up dotenv

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,3 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- * Sample Eslint config for NodeJS ExpressJS MongoDB project
- */
 module.exports = {
   env: { es2020: true, node: true },
   extends: ["eslint:recommended"],
@@ -27,7 +21,6 @@ module.exports = {
     "space-before-blocks": ["error", "always"],
     "object-curly-spacing": [1, "always"],
     indent: ["warn", 2],
-    semi: [1, "never"],
     quotes: ["error", "double"],
     "array-bracket-spacing": 1,
     "linebreak-style": 0,
--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,37 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import { MongoClient, ServerApiVersion } from "mongodb";
+import { env } from "~/config/environment";
+
+// Khởi tạo đối tượng trelloDatabaseInstance ban đầu là null vì chúng ta chưa connect db
+let trelloDatabaseInstance = null;
+
+// Khởi tạo đối tượng mongoClientInstance để connect tới db
+const mongoClientInstance = new MongoClient(env.MONGODB_URL, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+// Kết nối tới database
+export const CONNECT_DB = async () => {
+  // Gọi kết nối đến MongoDB Atlas với URL đã khai báo trong thân của mongoClientInstance
+  await mongoClientInstance.connect();
+
+  // Kết nối thành công thì lấy Database theo tên và gán ngược lại vào biến trelloDatabaseInstance
+  trelloDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME);
+};
+
+// Hàm GET_DB không async này có nhiệm vụ export ra cái trelloDatabaseInstance sau khi đã connect thành công tới MongoDB
+// để chúng ta sử dụng lại ở nhiều nơi khác nhau
+// Lưu ý: phải đảm bảo chỉ luôn gọi GET_DB sau khi đã kết nối thành công tới MongoDB
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance)
+    throw new Error("Must connect to database first!");
+
+  return trelloDatabaseInstance;
+};
+
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close();
+};
